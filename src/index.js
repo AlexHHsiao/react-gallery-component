b4c0accb23151c34ad9f4b29127a55c70242fd46
Alex Xiao <alex.hongquan.xiao@gmail.com>
2019-09-04 16:17:26 -0700

    fix build issue

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import React from "react";
 const ReactColorSquare = props => {
     const { width, height, color, text } = props;
     return (
-        <div
-            style={{
+        <div style={{
                 width: width || 100,
                 height: height || 100,
                 backgroundColor: color || "blue"
